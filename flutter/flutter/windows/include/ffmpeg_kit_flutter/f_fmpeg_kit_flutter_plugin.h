f5bbb7da99abd184b838bd2556c32e3b97662855
Julian Pscheid <julian@pscheid.com>
2025-08-01 10:29:43 -0700

    Add workaround header for Flutter codegen typo (f_fmpeg vs ffmpeg)

---
+++ flutter/flutter/windows/include/ffmpeg_kit_flutter/f_fmpeg_kit_flutter_plugin.h
@@ -0,0 +1,10 @@
+#ifndef FLUTTER_PLUGIN_F_FMPEG_KIT_FLUTTER_PLUGIN_H_
+#define FLUTTER_PLUGIN_F_FMPEG_KIT_FLUTTER_PLUGIN_H_
+
+// This file exists to work around a Flutter code generation bug that creates
+// an incorrect filename with an extra 'f_' prefix and lowercase 'f'
+// The actual implementation is in ffmpeg_kit_flutter_plugin.h
+
+#include "ffmpeg_kit_flutter_plugin.h"
+
+#endif  // FLUTTER_PLUGIN_F_FMPEG_KIT_FLUTTER_PLUGIN_H_
